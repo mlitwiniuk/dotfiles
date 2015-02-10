b5cedc24557d69ba2005a8ff1a05d15a0826c294
Maciej Litwiniuk <mlitwiniuk@inferno.local>
2015-02-10 09:44:40 +0100

    fix symlinking in installation, uncomment vundle and oh-my-zsh installation

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 dotfiles=(tmux.conf vimrc gemrc gitignore gitconfig gvimrc)
 for item in ${dotfiles[*]}
 do
-  ln -s `pwd`$item ~/.$item
+  ln -s `pwd`/$item ~/.$item
 done
 
-# wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+wget --no-check-certificate http://install.ohmyz.sh -O - | sh
 
 echo "source `pwd`/zshrc" >> ~/.zshrc
 
-# git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim +PluginInstall +qall
 
